1732dcfe889c1e9ffb968d5fe468342775829693
CarlitosDev2000 <carlos.cordova.perez.2000@gmail.com>
2023-08-02 01:33:21 -0500

    carlos update 1/08/23

--- app/src/main/java/com/carloscordova/my_dialog_exercise/MainActivity.java
+++ app/src/main/java/com/carloscordova/my_dialog_exercise/MainActivity.java
@@ -20,14 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText emailEditText;
     private List<String> list;
 
-    private Toast toast;
     @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "++ ON CREATE ++");
         list = new ArrayList<>();
-        toast = new Toast(this);
         setContentView(R.layout.activity_main);
         saveButton = findViewById(R.id.save_data_button);
         showRecordsButton = findViewById(R.id.show_button);
@@ -35,12 +33,18 @@ public class MainActivity extends AppCompatActivity {
         ageEditText = findViewById(R.id.age_option);
         nameEditText = findViewById(R.id.name_option);
         emailEditText = findViewById(R.id.email_option);
-        saveButton.setOnClickListener(v -> saveFormValues());
-        showRecordsButton.setOnClickListener(view -> showDialod());
+        saveButton.setOnClickListener(v -> {
+            saveFormValues();
+
+        });
+        showRecordsButton.setOnClickListener(view -> showDialog());
         cleanRecordsButton.setOnClickListener(v -> {
-            toast.setText("Deleted records : " + list.size());
-            toast.show();
+            if (list.isEmpty()){
+                showToast("no records to show");
+            }else
+            showToast("Deleted records" + list.size());
             // TODO: do something with the variable list to clear all the items;
+            list.clear();
             assert list.isEmpty();
         });
     }
@@ -61,33 +65,34 @@ public class MainActivity extends AppCompatActivity {
         if(isValidEmail(emailString)) {
             isEmailValid = true;
         } else {
-            toast.setText("error message here for invalid email");
-            toast.show();
+            emailEditText.setError("put your true email");
         }
         if(isValidName(nameString)) {
             isNameValid = true;
         } else {
-            toast.setText("error message here for invalid name");
-            toast.show();
+            nameEditText.setError("put correct your name");
         }
         if(isAgeValid(ageString)) {
             isAgeValid = true;
         } else {
-            toast.setText("error message here for invalid age");
-            toast.show();
+            ageEditText.setError("put your real age");
         }
         if(isNameValid && isEmailValid && isAgeValid) {
             list.add(nameString + " : " + emailString + " : " + ageString);
+            showToast("Added records.");
             clearEditTextFields();
         }
     }
 
-    private void showDialod() {
+    private void showDialog() {
         StringBuilder sb = new StringBuilder();
         list.forEach( str-> {
             sb.append(str);
             sb.append("\n");
         });
+        if (sb.length() == 0) {
+            sb.append("No hay registros disponibles");
+        }
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Records");
         builder.setMessage(sb.toString());
@@ -100,19 +105,39 @@ public class MainActivity extends AppCompatActivity {
         //TODO
         // validate here if email contains [@] and has at least one [.] character and length is at least 5 characters,
         // then resolve as true otherwise is false
-        return false;
+        if (!email.contains("@") && !email.contains(".")) {
+            showToast("Falta los simbolos @ o . en el email.");
+            return false;
+        } if (email.length() < 5 || email.length() > 40) {
+            showToast("El email debe tener entre 5 y 40 caracteres.");
+            return false;
+        }else
+            return true;
     }
     private boolean isValidName(String name){
         //TODO
         // validate here if name length is not empty , and has at least 3 characters
         // then resolve as true otherwise is false
-        return false;
+        if (name.length() >= 3 && name.length() <= 20){
+            return true;
+        }else
+            showToast("nombre debe tiene que tener de 3 a 20 caracteres");
+            return false;
     }
     private boolean isAgeValid(String age){
         //TODO
         // validate here if age is greater of equals thatn 18 and less than 99
         // then resolve as true otherwise is false
         // This is a string you need to parse it from string to int
-        return false;
+        try {
+            int ageValues = Integer.parseInt(age);
+            return ageValues >= 18 && ageValues < 99 && !age.isEmpty();
+        } catch (NumberFormatException ega){
+            showToast("numero incorrecto");
+            return false;
+        }
+    }
+    private void showToast(String toast) {
+        Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
     }
 }
